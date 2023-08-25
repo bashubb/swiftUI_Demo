b25c33b8003a25d9424ea7e15a93c803fb725a12
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-08-25 13:34:44 +0200

    Path intro

---
+++ SwiftUIViewSpecialist/Shapes and other views /Path1.swift
@@ -0,0 +1,42 @@
+//
+//  Path1.swift
+//  SwiftUIViewSpecialist
+//
+//  Created by HubertMac on 25/08/2023.
+//
+
+import SwiftUI
+
+struct Path1: View {
+    var body: some View {
+        VStack {
+            VStack {
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            }
+            Triangle()
+            .fill(Color.green)
+            .padding()
+        }
+    }
+}
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: 0, y: 0))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+            path.closeSubpath()
+        }
+    }
+}
+
+struct Path1_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            Path1()
+            Triangle()
+        }
+    }
+}
+
